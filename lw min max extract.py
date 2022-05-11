94ca6710b1fa334ce7d3fb42818a654ac4becd67
AlastairFrench <105351997+AlastairFrench@users.noreply.github.com>
2022-05-11 15:47:03 +0100

    Update lw min max extract.py

--- lw min max extract.py
+++ lw min max extract.py
@@ -14,31 +14,31 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #path to folder in which the files you're extracting are kept - must be /
-folder = '/Users/FA10041/Documents/Camera Windows/Powerview/Lightweight/reports/'
+folder = '/Users/FA10041/'
 
 #creates a list of strings of filenames for all .xls in a folder
 for root, dirs, files in os.walk(folder):
     xlsfiles=[ _ for _ in files if _.endswith('.xls') ]
 
 #function to extract optical data from powerview report
-def read_pv_data(name):
+def read_excel_data(name):
     
     workbook = xlrd.open_workbook('C:' + folder + '/' + name)
     general_sheet = workbook.sheet_by_name('General')
-    h_sheet = workbook.sheet_by_name('Camera Viewports 2')
-    v_sheet = workbook.sheet_by_name('Camera Viewports 1')
+    sheet1 = workbook.sheet_by_name('Sheet 1')
+    sheet2 = workbook.sheet_by_name('Sheet 2')
     
     name = name
     
-    angle = general_sheet.cell(10, 1).value
+    general = general_sheet.cell(10, 1).value
        
-    h_min = h_sheet.cell(12, 2).value
-    h_max = h_sheet.cell(12, 3).value
+    x_min = sheet1.cell(12, 2).value
+    x_max = sheet1.cell(12, 3).value
     
-    v_min = v_sheet.cell(12, 2).value
-    v_max = v_sheet.cell(12, 3).value
+    y_min = sheet2.cell(12, 2).value
+    y_max = sheet2.cell(12, 3).value
     
-    return [name, angle, h_min, h_max, v_min, v_max]
+    return [name, general, x_min, x_max, y_min, y_max]
 
 #creates df of extracted values    
 df = pd.concat([pd.DataFrame(read_pv_data(xlsfile)).T for xlsfile in xlsfiles])
@@ -46,103 +46,82 @@ df.reset_index(inplace=True)
 df.drop('index', axis=1, inplace=True)
 
 #adds column titles
-df.columns = ['name', 'angle', 'h_min', 'h_max', 'v_min', 'v_max']
+df.columns = ['name', 'general', 'x_min', 'x_max', 'y_min', 'y_max']
 
-#tidy angle column
-df['angle'] = df['angle'].str[:-1]
-df['angle'] = df['angle'].astype(float)
+#tidy general column
+df['general'] = df['general'].str[:-1]
+df['general'] = df['general'].astype(float)
 
-#tidy name column and make model column
-df['model'] = df['name'].str[:5]
+#tidy name column and make type column
+df['type'] = df['name'].str[:5]
 df['name'] = df['name'].str[54:]
 df['name'] = df['name'].str.rstrip('.xls')
 
-#make glass no. and xc column
-df['glass no.'] = df['name'].str[-3:]
-df['glass no.'] = df['glass no.'].astype(int)
-df['xc'] = df['name'].str[-8:]
-df['xc'] = df['xc'].str[:4]
-df['xc'] = df['xc'].astype(float)
+#make sample no. and a z column
+df['sample no.'] = df['name'].str[-3:]
+df['sample no.'] = df['glass no.'].astype(int)
+df['z'] = df['name'].str[-8:]
+df['z'] = df['z'].str[:4]
+df['z'] = df['z'].astype(float)
 
-#adding true/false columns for construction
-def construct_columns(construct):
-    df[construct] = df['name'].str.contains(construct, case=False)
+#adding true/false columns for info
+def info_columns(info):
+    df[info] = df['name'].str.contains(info, case=False)
     return df.head()
 
-constructs = ['S4', '18']
+info = ['A', 'B']
 
-for construct in constructs:
-    construct_columns(construct)
+for info in info:
+    info_columns(info)
     
 ############################################################################################################
 #swarmplots
 ############################################################################################################
 
-#creating just one construction column for the legend names
-df_plot = df[['glass no.', 'model', 'xc', 'h_min', 'h_max', 'v_min', 'v_max', 'S4', '18']].copy()
+#creating just one info column for the legend names
+df_plot = df[['sample no.', 'type', 'z', 'x_min', 'x_max', 'y_min', 'y_max', 'A', 'B']].copy()
 
-df_plot = df_plot.join((~df_plot[['S4', '18']]).add_prefix('inv_'))
-df_plot[['S4', '18', 'inv_S4', 'inv_18']]
-df_plot.rename(columns={'S4': 'S2+S4 IRR', '18': '1.8/1.4mm', 'inv_S4': 'S2 only', 'inv_18': '2.1/1.6mm'}, inplace=True)
+df_plot = df_plot.join((~df_plot[['A', 'B']]).add_prefix('inv_'))
+df_plot[['A', 'B', 'inv_A', 'inv_B']]
 
-constructs2 = ['S2+S4 IRR', 'S2 only', '1.8/1.4mm', '2.1/1.6mm']
+info2 = ['A', 'B', 'inv_A', 'inv_B']
 
-df_plot[constructs2] = df_plot[constructs2].where(df_plot != True, df_plot.columns.to_series(), axis=1)
-df_plot[constructs2] = df_plot[constructs2].replace(False,'')
+df_plot[info2] = df_plot[info2].where(df_plot != True, df_plot.columns.to_series(), axis=1)
+df_plot[info2] = df_plot[info2].replace(False,'')
 
-df_plot['construction'] = df_plot[constructs2].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
+df_plot['info'] = df_plot[info2].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
 
-df_plot = df_plot.drop(constructs2, 1)
+df_plot = df_plot.drop(info2, 1)
 
 
-#combining data for plots in one df
-df_min = df_plot[['model', 'xc', 'construction','h_min', 'v_min']]
-df_min.columns = ['Model', 'Cross Curve (mm)', 'Construction', 'Horizontal Optical Power (mDpt)', 'Vertical Optical Power (mDpt)']
-df_max = df_plot[['model', 'construction', 'h_max', 'v_max']]
-df_max.columns = ['Model', 'Construction', 'Horizontal Optical Power (mDpt)', 'Vertical Optical Power (mDpt)']
+#combining data for plots in one df so that all x and y values are in the same columns
+df_min = df_plot[['type', 'z', 'info','x_min', 'y_min']]
+df_max = df_plot[['type', 'info', 'x_max', 'y_max']]
 
 df_plot_final = pd.concat((df_min, df_max), axis=0)
 
-#can use this to filter by consturction
-unique_constructs = df_plot_final['Construction'].unique().tolist()
+#can use this to filter by info
+unique_infos = df_plot_final['info'].unique().tolist()
 
 
 #plotting data
+
+#swarm plot
 sns.set_theme(style='whitegrid')
 sns.set(rc={"figure.figsize":(14, 8)})
-g = sns.swarmplot(x='Construction', y='Vertical Optical Power (mDpt)', data=df_plot_final.loc[df_plot_final['Construction'].isin(unique_constructs)], dodge=True, size=7)
+g = sns.swarmplot(x='info', y='x', data=df_plot_final.loc[df_plot_final['info'].isin(unique_info)], dodge=True, size=7)
 loc, labels = plt.xticks()
 g.set_xticklabels(labels, rotation=45, fontsize=14)
-g.set_xlabel('Construction', fontweight = 'bold', fontsize=16)
-g.set_ylabel('Vertical Optical Power (mDpt)', fontweight = 'bold', fontsize=16)
+g.set_xlabel('Info', fontweight = 'bold', fontsize=16)
+g.set_ylabel('x', fontweight = 'bold', fontsize=16)
 
-sns.set_theme(style='whitegrid')
-sns.set(rc={"figure.figsize":(14, 8)})
-g = sns.swarmplot(x='Construction', y='Horizontal Optical Power (mDpt)', data=df_plot_final.loc[df_plot_final['Construction'].isin(unique_constructs)], dodge=True, size=7)
-loc, labels = plt.xticks()
-g.set_xticklabels(labels, rotation=45, fontsize=14)
-g.set_xlabel('Construction', fontweight = 'bold', fontsize=16)
-g.set_ylabel('Horizontal Optical Power (mDpt)', fontweight = 'bold', fontsize=16)
-
-############################################################################################################
-#scatter of xc vs min vert
-############################################################################################################
+#scatter of z vs x
 sns.set(rc={"figure.figsize":(12, 8)})
-g = sns.scatterplot(data=df_min, x='Cross Curve (mm)', y='Vertical Optical Power (mDpt)', hue='Construction')
+g = sns.scatterplot(data=df_min, x='z', y='x', hue='Construction')
 g.legend(fontsize='large', title_fontsize='40')
-g.set_xlabel('Cross Curve (mm)', fontweight = 'bold', fontsize=16)
-g.set_ylabel('Minimum Vertical Optical Power (mDpt)', fontweight = 'bold', fontsize=16)
+g.set_xlabel('z', fontweight = 'bold', fontsize=16)
+g.set_ylabel('x', fontweight = 'bold', fontsize=16)
 
-############################################################################################################
-#vert data analysis
-############################################################################################################
-average_vert_min = {}
-for i in range(len(unique_constructs)):
-    average_vert_min[i] = {unique_constructs[i]:df_min['Vertical Optical Power (mDpt)'].loc[df_min['Construction'] == unique_constructs[i]].mean()}
-
-std_vert_min = {}
-for i in range(len(unique_constructs)):
-    std_vert_min[i] = {unique_constructs[i]:df_min['Vertical Optical Power (mDpt)'].loc[df_min['Construction'] == unique_constructs[i]].std()}
     
 
 
